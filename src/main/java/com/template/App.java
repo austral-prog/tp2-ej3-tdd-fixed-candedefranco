c91897d474a1fde29d7f96dc5fdb2e9a91cafcf4
Cande De Franco <candedefranco@Candes-MacBook-Pro.local>
2024-08-16 12:55:27 -0300

    resolvi el ej 3 del tp2.

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -7,8 +7,29 @@ public class App {
 
 
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+       if (amount > 0 && accounts.containsKey(account) == true){
+        int newAmount = accounts.get(account)+amount;
+        accounts.put(account, newAmount );
+       }
+       return accounts;
     }
 
+    public static Map<String, Integer> extract(Map<String, Integer> accounts, String account, int amount) {
+        if (amount > 0 && accounts.containsKey(account) == true && accounts.get(account) > 0 ) {
+            int newAmount = accounts.get(account) - amount;
+            accounts.put(account, newAmount);
+        }
+        return accounts;
+    }
+
+    public static Map<String, Integer> transfer(Map<String, Integer> accounts, String account1, String account2, int amount) {
+        if (accounts.containsKey(account1) == true && accounts.containsKey(account2) == true && accounts.get(account1) > 0 && amount > 0 && !account1.equals(account2)) {
+            int NewAmount1 = accounts.get(account1) - amount;
+            int NewAmount2 = accounts.get(account2) + amount;
+            accounts.put(account1, NewAmount1);
+            accounts.put(account2, NewAmount2);
+        }
+        return accounts;
+    }
 
 }
--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -1,18 +1,144 @@
 package com.template;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
 
 import static com.template.App.deposit;
+import static com.template.App.extract;
+import static com.template.App.transfer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
 
     @Test
     void testDeposit() {
-        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
         Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
-        assertEquals(true, true);
+        assertEquals(15, updatedAccounts.get("mati"));
     }
+
+    @Test
+    void testNegativeDeposit() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "mati", -5);
+        assertEquals(10, updatedAccounts.get("mati"));
+    }
+
+    @Test
+    void testNonExistentDeposit() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "tomi", 5);
+        assertEquals(10, updatedAccounts.get("mati"));
+    }
+
+    @Test
+    void testExtract() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = extract(accounts, "mati", 5);
+        assertEquals(5, updatedAccounts.get("mati"));
+
+    }
+
+    @Test
+    void testNegativeExtract() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = extract(accounts, "mati", -5);
+        assertEquals(10, updatedAccounts.get("mati"));
+    }
+
+    @Test
+    void testNonExistentBalance() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 0);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = extract(accounts, "mati", 5);
+        assertEquals(0, updatedAccounts.get("mati"));
+
+    }
+
+    @Test
+    void testNonExistentExtract() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updatedAccounts = extract(accounts, "tomi", 5);
+        assertEquals(10, updatedAccounts.get("mati"));
+
+    }
+
+    @Test
+    void testTransfer() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "juan", 5);
+        assertEquals(5, updatedAccounts.get("mati"));
+        assertEquals(25, updatedAccounts.get("juan"));
+    }
+
+    @Test
+    void testNonExistentBothBalances() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 0);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "juan", 5);
+        assertEquals(0, updatedAccounts.get("mati"));
+        assertEquals(20, updatedAccounts.get("juan"));
+    }
+
+    @Test
+    void testNonExistentTransfer1() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "tomi", "mati", 5);
+        assertEquals(10, updatedAccounts.get("mati"));
+        assertEquals(20, updatedAccounts.get("juan"));
+    }
+
+    @Test
+    void testNonExistentTransfer2() {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "tomi", 5);
+        assertEquals(10, updatedAccounts.get("mati"));
+        assertEquals(20, updatedAccounts.get("juan"));
+    }
+
+    @Test
+    void testNegativeTransfer () {
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "juan", -5);
+        assertEquals(10, updatedAccounts.get("mati"));
+        assertEquals(20, updatedAccounts.get("juan"));
+
+    }
+
+    @Test
+    void TestSameAccountTransfer (){
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "mati", 5);
+        assertEquals(10, updatedAccounts.get("mati"));
+        assertEquals(20, updatedAccounts.get("juan"));
+    }
+
 }
